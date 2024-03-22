eeea9eee8604f81784e73c62b47aa70e95c3936f
Jade Liang <jadeliang02@hotmail.com>
2024-03-22 12:22:34 +0800

    Add QuizPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import QuizPage from './QuizPage';
-//import LoginForm from './LoginPage';
 
 function App() {
-  <Routes>
-        <Route path="/LoginPage" element={<LoginPage />} />
-        <Route path="/QuizPage" element={<QuizPage />} />
-  </Routes>
-};
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={loggedIn ? <QuizPage /> : <LoginPage onLogin={handleLogin} />}
+        />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
